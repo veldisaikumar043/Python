bde47f29dd091437ff19f0c4c8c898f8f06a2959
veldisaikumar <112003304+veldisaikumar@users.noreply.github.com>
2022-09-18 19:25:59 +0530

    tested

---
+++ practice.py
@@ -0,0 +1,60 @@
+# a = 10
+# print(a)
+# b = 20.54
+# print(b)
+# c = True
+# print(c)
+# d = "Iamthebestsaikumar"
+# print(d[:8:2])
+
+# lst=['india','china','italy']
+# lst.append('canada')
+# del(lst[2])
+# lst.insert(int(len(lst)/2),'usa')
+# print(lst)
+# a=;
+# print(a)
+
+# lst={1:"onions",2:"lettuce",3:"tomato",4:"olives",5:"peppers",6:"tomatoes"}
+#
+# print(lst)
+# first=int(input("enter first choice "))
+# print(lst[first])
+# second=int(input("enter second choice "))
+# print(lst[second])
+# third=int(input("enter third choice "))
+# print(lst[third])
+#
+# howMany=int(input("enter no.of Sandwiches you want "))
+# total=5*howMany
+# print(f"Total = {total}$")
+
+#GRADING APPLICATION
+# maths=int(input("enter maths marks "))
+# physics=int(input("enter physics marks "))
+# chemistry=int(input("enter chemistry marks "))
+# if maths>=35 and physics>=35 and chemistry>=35:
+#     print("you passed the exams")
+#     avg=(maths+physics+chemistry)/3
+#     if avg<=59:
+#         print("Grade: C")
+#     elif avg<=69:
+#         print("Grade: B")
+#     else:
+#         print("Grade: A")
+#
+# else:
+#     print("you failed the exam")
+
+# #PRINT 1 TO 100 SKIP 10 MULIPLES & BREAK CROSSES 100
+# n=int(input("enter a number"))
+# i=0
+# while(i<=n):
+#     i+=1
+#     if i%10==0:
+#         continue
+#     elif i<=100:
+#         print(i)
+#     else:
+#         break
+
