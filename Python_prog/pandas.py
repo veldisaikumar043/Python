4afb6d95960661d8e947c05c0212a559303f2c31
veldisaikumar <112003304+veldisaikumar@users.noreply.github.com>
2022-11-01 16:12:15 +0530

    searching and delete of stack

--- Python_prog/pandas.py
+++ Python_prog/pandas.py
@@ -15,14 +15,29 @@
 #
 # print(df.to_string())
 
+# DICTIONARY TO DATAFRAME
 # import pandas as pd
-#
 # dataF = {
 #   "calories": [420, 380, 390],
 #   "duration": [50, 40, 45]
 # }
-# df = pd.DataFrame(dataF)
+# df = pd.DataFrame.from_dict(dataF)
 # print(df)
 
 
+# LIST TO DATAFRAME
+# import pandas as pd
+# lst = ['Geeks', 'For', 'Geeks', 'is',
+#        'portal', 'for', 'Geeks']
+# df = pd.DataFrame(lst)
+# print(df)
 
+# TUPLE TO DATAFRAME
+# from pandas import DataFrame as df
+# tpl = ("Python", "Machine Learning", "Data Science", "Data Analyst")
+# result = df(tpl, index=["a", "b", "c", "d"], columns=["Languages"])
+# print(result)
+
+
+
+print("completed")
